4f47d48477a0e06cd589d2ec5a6cb037e32150ea
paraslov <paraslovjm@gmail.com>
2024-07-14 14:13:12 +0500

    s4w1 (feat): prepare to deploy to vercel

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppSettings } from './settings/appSettings'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
   await app.listen(AppSettings.PORT)
 }
 bootstrap()

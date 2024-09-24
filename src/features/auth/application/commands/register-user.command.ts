3839416a62f1352e03fe5cbc8721edbd0565176b
paraslov <paraslovjm@gmail.com>
2024-09-24 21:54:12 +0500

    s4w2 (feat): update register-user.command.ts

--- src/features/auth/application/commands/register-user.command.ts
+++ src/features/auth/application/commands/register-user.command.ts
@@ -1,5 +1,10 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
-import { CreateUserDto } from '../../../users'
+import { CreateUserDto, User } from '../../../users'
+import { CryptService } from '../../../../common/services'
+import { v4 as uuidv4 } from 'uuid'
+import { add } from 'date-fns'
+import { UsersRepository } from '../../../users/infrastructure/users.repository'
+import { EmailSendManager } from '../../../../common/manager/email-send.manager'
 
 export class RegisterUserCommand {
   constructor(public readonly createUserDto: CreateUserDto) {}
@@ -7,9 +12,44 @@ export class RegisterUserCommand {
 
 @CommandHandler(RegisterUserCommand)
 export class RegisterUserHandler implements ICommandHandler<RegisterUserCommand> {
+  constructor(
+    private readonly cryptService: CryptService,
+    private readonly usersRepository: UsersRepository,
+    private readonly emailSendManager: EmailSendManager,
+  ) {}
+
   async execute(command: RegisterUserCommand) {
     const { createUserDto } = command
+    const { login, email, password } = createUserDto
+    const passwordHash = await this.cryptService.generateHash(password)
+    const confirmationCode = uuidv4()
 
-    return createUserDto
+    const userToRegister: User = {
+      userData: {
+        login,
+        email,
+        passwordHash,
+        createdAt: new Date().toISOString(),
+      },
+      userConfirmationData: {
+        confirmationCode,
+        confirmationCodeExpirationDate: add(new Date(), {
+          hours: 1,
+          minutes: 1,
+        }),
+        isConfirmed: false,
+      },
+    }
+
+    await this.usersRepository.saveUser(userToRegister)
+
+    try {
+      const mailInfo = await this.emailSendManager.sendRegistrationEmail(email, confirmationCode)
+      console.log('@> Information::mailInfo: ', mailInfo)
+    } catch (err) {
+      console.error('@> Error::emailManager: ', err)
+    }
+
+    return true
   }
 }

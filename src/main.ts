31b07a1272fda3751b75ebb46f97270c1b34527f
paraslov <paraslovjm@gmail.com>
2024-09-30 21:26:03 +0500

    s4w2 (feat): use configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { appSettings } from './settings/app.settings'
 import { applyAppSettings } from './settings/apply.app.settings'
+import { ConfigService } from '@nestjs/config'
+import { ConfigurationType } from './settings/configuration'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   applyAppSettings(app)
 
-  await app.listen(appSettings.api.APP_PORT, () => {
-    console.log('App starting listen port: ', appSettings.api.APP_PORT)
-    console.log('ENV: ', appSettings.env.getEnv())
+  const configService = app.get(ConfigService<ConfigurationType, true>)
+  const apiSettings = configService.get('apiSettings', { infer: true })
+  const environmentSettings = configService.get('environmentSettings', {
+    infer: true,
+  })
+  const port = apiSettings.APP_PORT
+
+  await app.listen(port, () => {
+    console.log('App starting listen port: ', port)
+    console.log('ENV: ', environmentSettings.currentEnv)
   })
 }
 bootstrap()

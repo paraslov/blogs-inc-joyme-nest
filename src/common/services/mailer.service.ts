a665b322ab44c16f562526ba6fa163e1ebf82a31
paraslov <paraslovjm@gmail.com>
2024-09-30 21:45:58 +0500

    s4w2 (feat): refactor mailer.service.ts to use config service

--- src/common/services/mailer.service.ts
+++ src/common/services/mailer.service.ts
@@ -1,15 +1,18 @@
 import nodemailer from 'nodemailer'
 import { MailOptions } from 'nodemailer/lib/sendmail-transport'
-import { appSettings } from '../../settings/app.settings'
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { ConfigurationType } from '../../settings/configuration'
 
 @Injectable()
 export class MailerService {
+  constructor(private readonly configService: ConfigService<ConfigurationType>) {}
+
   private readonly transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: appSettings.api.SEND_MAIL_SERVICE_EMAIL,
-      pass: appSettings.api.SEND_MAIL_SERVICE_PASSWORD,
+      user: this.configService.get('mailerSettings').SEND_MAIL_SERVICE_EMAIL,
+      pass: this.configService.get('mailerSettings').SEND_MAIL_SERVICE_PASSWORD,
     },
   })
 
--- src/settings/api.settings.ts
+++ src/settings/api.settings.ts
@@ -4,5 +4,5 @@ import { EnvironmentVariable } from './configuration'
 export class ApiSettings {
   constructor(private environmentVariables: EnvironmentVariable) {}
   @IsNumber()
-  public readonly APP_PORT: number = Number(this.environmentVariables.PORT) || 3000
+  public readonly APP_PORT: number = Number(this.environmentVariables.APP_PORT) || 3000
 }

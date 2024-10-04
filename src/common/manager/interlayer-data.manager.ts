f4520bb49ff10deac1ee0281219676c773d4f31b
paraslov <paraslovjm@gmail.com>
2024-10-04 20:12:30 +0500

    s4w2 (feat): fix interlayer-data.manager.ts to correct dto

--- src/common/manager/interlayer-data.manager.ts
+++ src/common/manager/interlayer-data.manager.ts
@@ -1,34 +1,31 @@
 export class InterlayerDataManager<D = null> {
-  public data: D | null = null;
-  public extensions: InterlayerDataManagerExtension[];
-  public code = 0;
+  public data: D | null = null
+  public extensions: InterlayerDataManagerExtension[]
+  public code = 0
 
   constructor(data: D | null = null) {
-    this.data = data;
-    this.extensions = [];
+    this.data = data
+    this.extensions = []
   }
 
   public addData(data: D): void {
-    this.data = data;
+    this.data = data
   }
-  public addError(
-    message: string,
-    key: string | null = null,
-    code: number | null = null,
-  ): void {
-    this.code = code ?? 1;
-    this.extensions.push(new InterlayerDataManagerExtension(message, key));
+  public addError(message: string, field: string | null = null, code: number | null = null): void {
+    this.code = code ?? 1
+    this.extensions.push(new InterlayerDataManagerExtension(message, field))
   }
   public hasError(): boolean {
-    return this.code !== 0;
+    return this.code !== 0
   }
 }
 
 class InterlayerDataManagerExtension {
-  public readonly message: string;
-  public readonly key: string | null;
-  constructor(message: string, key: string | null = null) {
-    this.message = message;
-    this.key = key;
+  public readonly message: string
+  public readonly field: string | null
+
+  constructor(message: string, field: string | null = null) {
+    this.message = message
+    this.field = field
   }
 }

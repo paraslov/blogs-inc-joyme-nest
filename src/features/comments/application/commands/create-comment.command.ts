ce11930c8e9c2131ec828a985f265cfbaf791e77
paraslov <paraslovjm@gmail.com>
2024-10-14 22:01:17 +0500

    s4w2 (feat): add comments-comment.command.ts

---
+++ src/features/comments/application/commands/create-comment.command.ts
@@ -0,0 +1,36 @@
+import { CreateUpdateCommentDto } from '../../api/models/input/create-update-comment.dto'
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { Comment } from '../../domain/mongoose/comment.entity'
+import { CommentsRepository } from '../../infrastructure/comments.repository'
+
+export class CreateCommentCommand {
+  constructor(
+    public readonly createCommentDto: CreateUpdateCommentDto,
+    public readonly parentId: string,
+    public readonly userId: string,
+    public readonly userLogin: string,
+  ) {}
+}
+
+@CommandHandler(CreateCommentCommand)
+export class CreateCommentHandler implements ICommandHandler<CreateCommentCommand> {
+  constructor(private readonly commentsRepository: CommentsRepository) {}
+
+  execute(command: CreateCommentCommand) {
+    const { createCommentDto, parentId, userLogin, userId } = command
+
+    const newComment: Comment = {
+      parentId,
+      content: createCommentDto.content,
+      commentatorInfo: {
+        userId,
+        userLogin,
+      },
+      createdAt: new Date().toISOString(),
+      likesCount: 0,
+      dislikesCount: 0,
+    }
+
+    return this.commentsRepository.saveComment(newComment)
+  }
+}

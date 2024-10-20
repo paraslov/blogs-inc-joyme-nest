9f8227a18fdc6ae58cebf865e26c63a42cacb8b7
paraslov <paraslovjm@gmail.com>
2024-10-20 20:49:21 +0500

    s4w2 (feat): refactor query filters

--- src/features/users/api/models/input/filter-users.dto.ts
+++ src/features/users/api/models/input/filter-users.dto.ts
@@ -2,18 +2,11 @@ import { StandardInputFilters } from '../../../../../common/models/input/QueryIn
 import { IsOptional, IsString } from 'class-validator'
 
 export class FilterUsersDto extends StandardInputFilters {
-  constructor() {
-    super()
-
-    this.searchLoginTerm = ''
-    this.searchEmailTerm = ''
-  }
+  @IsOptional()
+  @IsString()
+  searchLoginTerm: string = ''
 
   @IsOptional()
   @IsString()
-  searchLoginTerm?: string
-
-  @IsOptional()
-  @IsString()
-  searchEmailTerm?: string
+  searchEmailTerm: string = ''
 }

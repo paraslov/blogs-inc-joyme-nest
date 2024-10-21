032936f2462b6f5975ebaa8d911800d29acfba85
paraslov <paraslovjm@gmail.com>
2024-10-21 07:33:58 +0500

    s4w2 (feat): add possible-user-id.decorator.ts

---
+++ src/base/decorators/index.ts
@@ -0,0 +1,5 @@
+import { CurrentUserId } from './current-user-id.decorator'
+import { Cookies } from './parse-cookies.decorator'
+import { PossibleUserId } from './possible-user-id.decorator'
+
+export { PossibleUserId, CurrentUserId, Cookies }
---
+++ src/base/decorators/possible-user-id.decorator.ts
@@ -0,0 +1,7 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common'
+
+export const PossibleUserId = createParamDecorator((data: unknown, context: ExecutionContext): string => {
+  const request = context.switchToHttp().getRequest()
+
+  return request.user?.userId
+})

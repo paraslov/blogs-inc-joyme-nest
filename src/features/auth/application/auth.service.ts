dd579c2633a86367bb48b9c56214da1242df1e3d
paraslov <paraslovjm@gmail.com>
2024-10-24 20:20:03 +0500

    s4w2 (fix): fixes of error handling and refresh token hardcode

--- src/features/auth/application/auth.service.ts
+++ src/features/auth/application/auth.service.ts
@@ -4,6 +4,10 @@ import { JwtService } from '@nestjs/jwt'
 import { AuthStrategiesDto } from '../api/models/utility/auth-strategies-dto'
 import { AuthRepository } from '../infrastructure/auth.repository'
 
+const REFRESH_TOKEN =
+  // eslint-disable-next-line max-len
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im5ld1VzZXIxIiwic3ViIjoiNjcwYjRiNDEzNjYwNjg4OWQ4NDBhODhiIiwiaWF0IjoxNzI5NjEzODEzLCJleHAiOjE3Mjk2MTQxMTN9.-HG3EeneipJqGMX24zmYvcU_X8oDoqS4WLZJjAklCpE'
+
 @Injectable()
 export class AuthService {
   constructor(
@@ -30,7 +34,7 @@ export class AuthService {
   async login(payload: AuthStrategiesDto) {
     return {
       accessToken: this.jwtService.sign(payload),
-      refreshToken: 'refresh_token',
+      refreshToken: REFRESH_TOKEN,
     }
   }
 }

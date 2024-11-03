c509815967c4c3b783aab594d2aa7d345135982b
paraslov <paraslovjm@gmail.com>
2024-11-03 20:46:05 +0500

    s4w3 (feat): add comments.e2e-spec.ts

--- src/features/posts/index.ts
+++ src/features/posts/index.ts
@@ -2,5 +2,6 @@ import { PostEntity } from './domain/mongoose/posts.entity'
 import { PostsQueryRepository } from './infrastructure/posts.query-repository'
 import { PostsMappers } from './infrastructure/posts.mappers'
 import { PostsMongooseModule } from './domain/mongoose/posts.entity'
+import { PostsTestManager } from './tests/utils/posts-test.manager'
 
-export { PostEntity, PostsQueryRepository, PostsMappers, PostsMongooseModule }
+export { PostEntity, PostsQueryRepository, PostsMappers, PostsMongooseModule, PostsTestManager }

1aad0a2a72ae339f7c8e616d1fde38ee5fa532fd
paraslov <paraslovjm@gmail.com>
2024-11-04 17:14:14 +0500

    s4w2 (feat): add create-device-session.command.ts

---
+++ src/features/devices/application/commands/create-device-session.command.ts
@@ -0,0 +1,69 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { DevicesService } from '../devices.service'
+import { Device } from '../../domain/mongoose/device.entity'
+import { DevicesRepository } from '../../infrastructure/devices.repository'
+import { InterlayerDataManager } from '../../../../common/manager'
+import { HttpStatusCodes } from '../../../../common/models'
+
+export class CreateDeviceSessionCommand {
+  constructor(
+    public readonly payload: {
+      userId: string
+      deviceName: string
+      ip: string
+      refreshToken: string
+    },
+  ) {}
+}
+
+@CommandHandler(CreateDeviceSessionCommand)
+export class CreateDeviceSessionCommandHandler implements ICommandHandler<CreateDeviceSessionCommand> {
+  constructor(
+    private deviceService: DevicesService,
+    private devicesRepository: DevicesRepository,
+  ) {}
+
+  async execute({ payload }: CreateDeviceSessionCommand) {
+    const { userId, refreshToken, ip, deviceName } = payload
+
+    const { decodedData, notice } = await this.checkTokenData(refreshToken)
+    if (notice.hasError()) {
+      return notice
+    }
+
+    const newDeviceSession: Device = {
+      ip,
+      deviceName,
+      userId,
+      deviceId: decodedData.deviceId,
+      iat: decodedData.iat,
+      exp: decodedData.exp,
+    }
+
+    const saveResult = await this.devicesRepository.saveDeviceSession(newDeviceSession)
+    if (!saveResult) {
+      notice.addError('Failed to create device session', null, HttpStatusCodes.EXPECTATION_FAILED_417)
+    }
+
+    return notice
+  }
+
+  async checkTokenData(refreshToken: string) {
+    const notice = new InterlayerDataManager()
+    const decodedData = this.deviceService.decodeRefreshToken(refreshToken)
+
+    if (!decodedData?.deviceId) {
+      notice.addError('No refresh token data', 'refreshToken', HttpStatusCodes.BAD_REQUEST_400)
+
+      return { decodedData, notice }
+    }
+    const hasCurrentDevice = await this.devicesRepository.getDeviceById(decodedData.deviceId)
+    if (hasCurrentDevice) {
+      notice.addError('Already has device session', 'refreshToken', HttpStatusCodes.BAD_REQUEST_400)
+
+      return { decodedData, notice }
+    }
+
+    return { decodedData, notice }
+  }
+}
--- src/features/devices/application/commands/index.ts
+++ src/features/devices/application/commands/index.ts
@@ -1,3 +1,4 @@
 import { DeleteOtherDevicesCommandHandler } from './delete-other-devices.command'
+import { CreateDeviceSessionCommandHandler } from './create-device-session.command'
 
-export const devicesCommandHandlers = [DeleteOtherDevicesCommandHandler]
+export const devicesCommandHandlers = [DeleteOtherDevicesCommandHandler, CreateDeviceSessionCommandHandler]

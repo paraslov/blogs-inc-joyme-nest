8fb829060dc4e2ae9944617dc5cea0f59b2a1b3d
paraslov <paraslovjm@gmail.com>
2024-11-04 10:39:53 +0500

    s4w2 (feat): add login.command.ts

---
+++ src/features/auth/application/commands/login.command.ts
@@ -0,0 +1,28 @@
+import { AuthStrategiesDto } from '../../api/models/utility/auth-strategies-dto'
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { AuthService } from '../auth.service'
+import { InterlayerDataManager } from '../../../../common/manager'
+
+export type TokensPair = {
+  accessToken: string
+  refreshToken: string
+}
+
+export class LoginCommand {
+  constructor(public authData: AuthStrategiesDto) {}
+}
+
+@CommandHandler(LoginCommand)
+export class LoginCommandHandler implements ICommandHandler<LoginCommand> {
+  constructor(private authService: AuthService) {}
+
+  async execute(command: LoginCommand) {
+    const { authData } = command
+    const notice = new InterlayerDataManager<TokensPair>()
+    const tokens = await this.authService.getTokens(authData)
+
+    notice.addData(tokens)
+
+    return notice
+  }
+}

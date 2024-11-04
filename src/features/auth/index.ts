6c3834b32cae2c62706401ea21af120a8d91779e
paraslov <paraslovjm@gmail.com>
2024-11-04 20:51:50 +0500

    s4w4 (tests): add devices.e2e-spec.ts

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,5 +1,6 @@
 import { AuthRequestDto } from './api/models/utility/auth-request.dto'
 import { JwtAuthGuard } from './application/guards/jwt-auth.guard'
 import { SaAuthGuard } from './application/guards/sa-auth.guard'
+import { AuthTestManager } from './tests/utils/auth-test.manager'
 
-export { JwtAuthGuard, SaAuthGuard, AuthRequestDto }
+export { JwtAuthGuard, SaAuthGuard, AuthRequestDto, AuthTestManager }

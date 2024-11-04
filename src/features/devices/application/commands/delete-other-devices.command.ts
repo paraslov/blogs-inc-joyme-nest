9abce11bca02cb77cfbb2a9999c62099439bfdd5
paraslov <paraslovjm@gmail.com>
2024-11-04 21:30:17 +0500

    s4w4 (feat): update delete-other-devices.command.ts

--- src/features/devices/application/commands/delete-other-devices.command.ts
+++ src/features/devices/application/commands/delete-other-devices.command.ts
@@ -1,8 +1,9 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
 import { DevicesRepository } from '../../infrastructure/devices.repository'
 import { InterlayerDataManager } from '../../../../common/manager'
-import { JwtService } from '@nestjs/jwt'
 import { DevicesService } from '../devices.service'
+import { HttpStatusCodes } from '../../../../common/models'
+import { DevicesQueryRepository } from '../../infrastructure/devices.query-repository'
 
 export class DeleteOtherDevicesCommand {
   constructor(public readonly refreshToken: string) {}
@@ -11,15 +12,28 @@ export class DeleteOtherDevicesCommand {
 @CommandHandler(DeleteOtherDevicesCommand)
 export class DeleteOtherDevicesCommandHandler implements ICommandHandler<DeleteOtherDevicesCommand> {
   constructor(
+    private devicesQueryRepository: DevicesQueryRepository,
     private devicesRepository: DevicesRepository,
     private deviceService: DevicesService,
-    private jwtService: JwtService,
   ) {}
 
   async execute({ refreshToken }) {
     const notice = new InterlayerDataManager()
 
     const decodedData = this.deviceService.decodeRefreshToken(refreshToken)
-    console.log('@> decoded data', decodedData)
+    if (!decodedData?.deviceId) {
+      notice.addError('No refresh token data', 'refreshToken', HttpStatusCodes.BAD_REQUEST_400)
+
+      return notice
+    }
+
+    const userDevices = await this.devicesQueryRepository.getAllDevices(decodedData.sub)
+    const devicesToDelete = userDevices
+      .filter((device) => device.deviceId !== decodedData.deviceId)
+      .map((dev) => dev.deviceId)
+
+    await this.devicesRepository.deleteOtherDevices(devicesToDelete)
+
+    return notice
   }
 }

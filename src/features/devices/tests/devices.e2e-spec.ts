20642cc8a5cede3db900bc327cc70ffb47050f72
paraslov <paraslovjm@gmail.com>
2024-11-05 11:19:53 +0500

    s4w2 (tests): update tests devices

--- src/features/devices/tests/devices.e2e-spec.ts
+++ src/features/devices/tests/devices.e2e-spec.ts
@@ -1,14 +1,17 @@
 import { INestApplication } from '@nestjs/common'
 import { UsersTestManager } from '../../users'
-import { initTestsSettings } from '../../../common/tests'
+import { initTestsSettings, wait } from '../../../common/tests'
 import { AuthTestManager } from '../../auth'
 import request from 'supertest'
 import { HttpStatusCodes } from '../../../common/models'
+import { DevicesTestManager } from './utils/devices.test-manager'
+import { JwtService } from '@nestjs/jwt'
 
 describe('auth', () => {
   let app: INestApplication
   let userTestManger: UsersTestManager
   let authTestManager: AuthTestManager
+  let devicesTestManager: DevicesTestManager
   let httpServer: any
 
   beforeAll(async () => {
@@ -18,6 +21,7 @@ describe('auth', () => {
       httpServer = result.httpServer
       userTestManger = result.userTestManger
 
+      devicesTestManager = new DevicesTestManager(new JwtService())
       authTestManager = new AuthTestManager(app)
     } catch (err) {
       console.log('@> auth tests error: ', err)
@@ -38,7 +42,7 @@ describe('auth', () => {
       .set({ Cookie: `refreshToken=${refreshToken}` })
       .expect(HttpStatusCodes.OK_200)
 
-    console.log('@> response: ', response.body)
+    expect(response.body?.[0].deviceId).toEqual(expect.any(String))
   })
 
   it('should delete all device session except current: ', async () => {
@@ -65,5 +69,41 @@ describe('auth', () => {
 
     expect(response.body.length).toBe(3)
     expect(responseAfterDelete.body.length).toBe(1)
+  })
+
+  it('should delete all device session except current: ', async () => {
+    await wait(1)
+    const { userRequestBody } = await userTestManger.createUser()
+    const { cookies } = await UsersTestManager.login(app, userRequestBody.login, userRequestBody.password)
+    await UsersTestManager.login(app, userRequestBody.login, userRequestBody.password)
+    const { cookies: cookiesToDelete } = await UsersTestManager.login(
+      app,
+      userRequestBody.login,
+      userRequestBody.password,
+    )
+    const refreshToken = authTestManager.getRefreshTokenFromResponseCookies(cookies)
+    const refreshTokenToDelete = authTestManager.getRefreshTokenFromResponseCookies(cookiesToDelete)
+    const { deviceId: deviceIdToDelete } = devicesTestManager.decodeRefreshToken(refreshTokenToDelete)
+
+    const response = await request(httpServer)
+      .get('/api/security/devices')
+      .set({ Cookie: `refreshToken=${refreshToken}` })
+      .expect(HttpStatusCodes.OK_200)
+
+    await request(httpServer)
+      .delete(`/api/security/devices/${deviceIdToDelete}`)
+      .set({ Cookie: `refreshToken=${refreshToken}` })
+      .expect(HttpStatusCodes.NO_CONTENT_204)
+
+    const responseAfterDelete = await request(httpServer)
+      .get('/api/security/devices')
+      .set({ Cookie: `refreshToken=${refreshToken}` })
+      .expect(HttpStatusCodes.OK_200)
+
+    console.log('@> responseAfterDelete.body: ', responseAfterDelete.body)
+
+    expect(response.body.length).toBe(3)
+    expect(responseAfterDelete.body.length).toBe(2)
+    expect(responseAfterDelete.body.some((device: any) => device.deviceId === deviceIdToDelete)).toBeFalsy()
   })
 })

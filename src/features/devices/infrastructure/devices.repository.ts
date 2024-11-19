951a4405569bc677036fa35ba6ddf2e9608f8392
paraslov <paraslovjm@gmail.com>
2024-11-19 10:43:59 +0500

    s5w1 (feat): add createDevicesTable method

--- src/features/devices/infrastructure/devices.repository.ts
+++ src/features/devices/infrastructure/devices.repository.ts
@@ -2,10 +2,36 @@ import { Injectable } from '@nestjs/common'
 import { Device, DeviceDocument } from '../domain/mongoose/device.entity'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
+import { DataSource } from 'typeorm'
 
 @Injectable()
 export class DevicesRepository {
-  constructor(@InjectModel(Device.name) private devicesModel: Model<Device>) {}
+  constructor(
+    @InjectModel(Device.name) private devicesModel: Model<Device>,
+    protected dataSource: DataSource,
+  ) {}
+
+  async createDevicesTable() {
+    const response = await this.dataSource.query(`
+    CREATE TABLE IF NOT EXISTS public.devices
+      (
+          device_id uuid NOT NULL,
+          device_name character varying(255) NOT NULL,
+          user_id uuid NOT NULL,
+          ip character varying(255) NOT NULL,
+          iat integer,
+          exp integer,
+          PRIMARY KEY (device_id)
+      );
+
+      ALTER TABLE IF EXISTS public.devices
+          OWNER to postgres;
+
+      GRANT ALL ON TABLE public.devices TO sa_sql_user;
+    `)
+
+    console.log('@> createDevicesTable response: ', response)
+  }
 
   async getDeviceById(deviceId: string) {
     const device = await this.devicesModel.findOne({ deviceId })

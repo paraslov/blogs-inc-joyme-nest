bcda0322c66ea887a7953c274fd90e27f85835ae
paraslov <paraslovjm@gmail.com>
2024-11-20 17:12:44 +0500

    s5w1 (feat): update auth.sql-repository.ts

--- src/features/auth/infrastructure/auth.sql-repository.ts
+++ src/features/auth/infrastructure/auth.sql-repository.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import { DataSource } from 'typeorm'
-import { UsersSqlEntity } from '../../users'
+import { UserInfo, UserSql } from '../../users'
 
 @Injectable()
 export class AuthSqlRepository {
   constructor(protected dataSource: DataSource) {}
 
-  async getUserByLoginOrEmail(loginOrEmail: string): Promise<UsersSqlEntity | null> {
+  async getUserByLoginOrEmail(loginOrEmail: string): Promise<UserSql | null> {
     const users = await this.dataSource.query(
       `
       SELECT id, login, email, password_hash, created_at
@@ -21,5 +21,23 @@ export class AuthSqlRepository {
     }
 
     return users[0]
+  }
+
+  async getUserConfirmationInfoByUserId(userId: string): Promise<UserInfo | null> {
+    const user = await this.dataSource.query(
+      `
+      SELECT user_id, confirmation_code, confirmation_code_expiration_date, is_confirmed,
+        password_recovery_code, password_recovery_code_expiration_date, is_password_recovery_confirmed
+          FROM public.users_confirmation_info
+          WHERE user_id = $1;
+    `,
+      [userId],
+    )
+
+    if (user?.length !== 1) {
+      return null
+    }
+
+    return user?.[0]
   }
 }

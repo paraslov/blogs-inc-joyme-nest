8b63860973629ab40dd1d5bc7a2f2f802e4b3bcc
paraslov <paraslovjm@gmail.com>
2024-11-20 10:51:33 +0500

    s5w1 (feat): update auth.query-repository.ts

--- src/features/auth/infrastructure/auth.query-repository.ts
+++ src/features/auth/infrastructure/auth.query-repository.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@nestjs/common'
-import { InjectModel } from '@nestjs/mongoose'
-import { User } from '../../users'
-import { Model } from 'mongoose'
 import { AuthMappers } from './auth.mappers'
+import { DataSource } from 'typeorm'
 
 @Injectable()
 export class AuthQueryRepository {
   constructor(
-    @InjectModel(User.name) private readonly usersModel: Model<User>,
     private readonly authMappers: AuthMappers,
+    protected dataSource: DataSource,
   ) {}
 
   async getMeInformation(userId: string) {
-    const user = await this.usersModel.findOne({ _id: userId })
+    const user = await this.dataSource.query(
+      `
+      SELECT id, email, login, password_hash, created_at
+        FROM public.users
+        WHERE id = $1;
+    `,
+      [userId],
+    )
 
-    return this.authMappers.mapDbToOutputDto(user)
+    return this.authMappers.mapDbToOutputDto(user?.[0])
   }
 }

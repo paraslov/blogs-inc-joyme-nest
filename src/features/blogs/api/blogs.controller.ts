220fdc353eba105c63eb3bea83dbffdc9add21db
paraslov <paraslovjm@gmail.com>
2024-11-24 18:35:42 +0500

    s5w1 (feat): refactor blogs controllers

--- src/features/blogs/api/blogs.controller.ts
+++ src/features/blogs/api/blogs.controller.ts
@@ -1,32 +1,14 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  NotFoundException,
-  Param,
-  Post,
-  Put,
-  Query,
-  UseGuards,
-} from '@nestjs/common'
-import { BlogsCommandService } from '../application/blogs.command.service'
-import { CreateBlogDto } from './models/input/create-blog.dto'
+import { Controller, Get, NotFoundException, Param, Query } from '@nestjs/common'
 import { StandardInputFilters, StandardInputFiltersWithSearchTerm } from '../../../common/models/input/QueryInputParams'
 import { ObjectIdValidationPipe } from '../../../base/pipes/object.id.validation.pipe'
 import { PostsQueryRepository } from '../../posts'
-import { CreateBlogPostDto } from './models/input/create-blog-post.dto'
 import { BlogsQueryRepository } from '../infrastructure/blogs.query-repository'
-import { SaAuthGuard } from '../../auth'
 import { PossibleUserId } from '../../../base/decorators'
 
 @Controller('blogs')
 export class BlogsController {
   constructor(
-    private readonly blogsService: BlogsCommandService,
     private readonly blogsQueryRepository: BlogsQueryRepository,
-
     private readonly postsQueryRepository: PostsQueryRepository,
   ) {}
 
@@ -50,47 +32,14 @@ export class BlogsController {
     return this.postsQueryRepository.getPostsList(query, { blogId: id, userId: currentUserId })
   }
 
-  @UseGuards(SaAuthGuard)
-  @HttpCode(201)
-  @Post()
-  create(@Body() createBlogDto: CreateBlogDto) {
-    return this.blogsService.createBlog(createBlogDto)
-  }
-
-  @UseGuards(SaAuthGuard)
-  @Post(':id/posts')
-  async createPostForBlog(
-    @Param('id', ObjectIdValidationPipe) blogId: string,
-    @Body() createBlogPostDto: CreateBlogPostDto,
-  ) {
-    const blog = await this.blogsQueryRepository.getBlogById(blogId)
+  @Get(':id')
+  async findOne(@Param('id', ObjectIdValidationPipe) id: string) {
+    const blog = await this.blogsQueryRepository.getBlogById(id)
 
     if (!blog) {
-      throw new NotFoundException(`Blog with ID ${blogId} not found`)
+      throw new NotFoundException(`Blog with ID ${id} not found`)
     }
 
-    return this.blogsService.createPost(createBlogPostDto, blogId, blog.name)
-  }
-
-  @UseGuards(SaAuthGuard)
-  @HttpCode(204)
-  @Put(':id')
-  async update(@Param('id', ObjectIdValidationPipe) id: string, @Body() updateBlogDto: CreateBlogDto) {
-    const updateResult = await this.blogsService.updateBlog(id, updateBlogDto)
-
-    if (!updateResult) {
-      throw new NotFoundException(`Couldn't update Blog with ID ${id}`)
-    }
-  }
-
-  @UseGuards(SaAuthGuard)
-  @HttpCode(204)
-  @Delete(':id')
-  async remove(@Param('id', ObjectIdValidationPipe) id: string) {
-    const deleteResult = await this.blogsService.deleteBlog(id)
-
-    if (!deleteResult) {
-      throw new NotFoundException(`Couldn't delete Blog with ID ${id}`)
-    }
+    return blog
   }
 }
--- src/features/blogs/api/blogs.sa.controller.ts
+++ src/features/blogs/api/blogs.sa.controller.ts
@@ -1,14 +1,32 @@
-import { Controller, Get, NotFoundException, Param, Query } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common'
+import { BlogsCommandService } from '../application/blogs.command.service'
+import { CreateBlogDto } from './models/input/create-blog.dto'
 import { StandardInputFilters, StandardInputFiltersWithSearchTerm } from '../../../common/models/input/QueryInputParams'
 import { ObjectIdValidationPipe } from '../../../base/pipes/object.id.validation.pipe'
 import { PostsQueryRepository } from '../../posts'
+import { CreateBlogPostDto } from './models/input/create-blog-post.dto'
 import { BlogsQueryRepository } from '../infrastructure/blogs.query-repository'
+import { SaAuthGuard } from '../../auth'
 import { PossibleUserId } from '../../../base/decorators'
 
 @Controller('sa/blogs')
 export class BlogsSaController {
   constructor(
+    private readonly blogsCommandService: BlogsCommandService,
     private readonly blogsQueryRepository: BlogsQueryRepository,
+
     private readonly postsQueryRepository: PostsQueryRepository,
   ) {}
 
@@ -32,14 +50,49 @@ export class BlogsSaController {
     return this.postsQueryRepository.getPostsList(query, { blogId: id, userId: currentUserId })
   }
 
-  @Get(':id')
-  async findOne(@Param('id', ObjectIdValidationPipe) id: string) {
-    const blog = await this.blogsQueryRepository.getBlogById(id)
+  @UseGuards(SaAuthGuard)
+  @HttpCode(201)
+  @Post()
+  async create(@Body() createBlogDto: CreateBlogDto) {
+    const createdBlogId = await this.blogsCommandService.createBlog(createBlogDto)
+
+    return await this.blogsQueryRepository.getBlogById(createdBlogId)
+  }
+
+  @UseGuards(SaAuthGuard)
+  @Post(':id/posts')
+  async createPostForBlog(
+    @Param('id', ObjectIdValidationPipe) blogId: string,
+    @Body() createBlogPostDto: CreateBlogPostDto,
+  ) {
+    const blog = await this.blogsQueryRepository.getBlogById(blogId)
 
     if (!blog) {
-      throw new NotFoundException(`Blog with ID ${id} not found`)
+      throw new NotFoundException(`Blog with ID ${blogId} not found`)
     }
 
-    return blog
+    return this.blogsCommandService.createPost(createBlogPostDto, blogId, blog.name)
+  }
+
+  @UseGuards(SaAuthGuard)
+  @HttpCode(204)
+  @Put(':id')
+  async update(@Param('id', ObjectIdValidationPipe) id: string, @Body() updateBlogDto: CreateBlogDto) {
+    const updateResult = await this.blogsCommandService.updateBlog(id, updateBlogDto)
+
+    if (!updateResult) {
+      throw new NotFoundException(`Couldn't update Blog with ID ${id}`)
+    }
+  }
+
+  @UseGuards(SaAuthGuard)
+  @HttpCode(204)
+  @Delete(':id')
+  async remove(@Param('id', ObjectIdValidationPipe) id: string) {
+    const deleteResult = await this.blogsCommandService.deleteBlog(id)
+
+    if (!deleteResult) {
+      throw new NotFoundException(`Couldn't delete Blog with ID ${id}`)
+    }
   }
 }

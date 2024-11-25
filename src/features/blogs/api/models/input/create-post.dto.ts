a988b6dd8ed7d2f8a2abc085e2a2f1c8482b3916
paraslov <paraslovjm@gmail.com>
2024-11-25 11:55:34 +0500

    s5w1 (feat): copy post dto to blogs module

---
+++ src/features/blogs/api/models/input/create-post.dto.ts
@@ -0,0 +1,34 @@
+import { IsNumber, IsOptional, MaxLength, Min, Validate } from 'class-validator'
+import { Transform } from 'class-transformer'
+import { toMongoObjectId } from '../../../../../base/transformers/toMongoObjectId'
+import { TrimmedString } from '../../../../../base/decorators'
+import { IsBlogExistsConstraint } from '../../../application/decorators/is-blog-exists.decorator'
+
+export class CreatePostDto {
+  @TrimmedString({ required: true })
+  @MaxLength(30)
+  title: string
+
+  @TrimmedString({ required: true })
+  @MaxLength(100)
+  shortDescription: string
+
+  @TrimmedString({ required: true })
+  @MaxLength(1000)
+  content: string
+
+  @TrimmedString({ required: true })
+  @Validate(IsBlogExistsConstraint)
+  @Transform(toMongoObjectId)
+  blogId: string
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  likesCount?: number
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  dislikesCount?: number
+}

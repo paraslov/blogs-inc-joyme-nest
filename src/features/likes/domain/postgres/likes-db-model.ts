f30a5d2ec412a43dd22568ebf4f7942e3fe81926
paraslov <paraslovjm@gmail.com>
2024-12-12 20:58:33 +0500

    s6w1 (feat): update LikeDbModel

--- src/features/likes/domain/postgres/likes-db-model.ts
+++ src/features/likes/domain/postgres/likes-db-model.ts
@@ -1,9 +1,20 @@
 import { LikeStatus } from '../../api/models/enums/like-status'
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 
+@Entity('likes')
 export class LikesDbModel {
+  @PrimaryColumn()
   parent_id: string
+
+  @Column({ type: 'varchar', length: 30 })
   status: LikeStatus
+
+  @CreateDateColumn()
   created_at: Date
+
+  @PrimaryColumn()
   user_id: string
+
+  @Column({ type: 'varchar', length: 10 })
   user_login: string
 }

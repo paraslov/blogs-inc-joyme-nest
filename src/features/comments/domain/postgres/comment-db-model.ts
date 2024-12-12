4f4eed06554351706e73a93643250fc91542ae14
paraslov <paraslovjm@gmail.com>
2024-12-12 20:45:04 +0500

    s6w1 (feat): update comment-db-model.ts

--- src/features/comments/domain/postgres/comment-db-model.ts
+++ src/features/comments/domain/postgres/comment-db-model.ts
@@ -1,10 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('comments')
 export class CommentDbModel {
+  @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @Column('uuid')
   parent_id: string
+
+  @Column({ type: 'varchar', length: 300 })
   content: string
+
+  @CreateDateColumn()
   created_at: string
+
+  @Column('uuid')
   user_id: string
+
+  @Column({ type: 'varchar', length: 10 })
   user_login: string
+
+  @Column({ type: 'integer', nullable: true })
   likes_count: number
+
+  @Column({ type: 'integer', nullable: true })
   dislikes_count: number
 }

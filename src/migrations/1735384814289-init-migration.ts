576a2f59bd9bea6f8ea3acc80314e30d5be61d18
paraslov <paraslovjm@gmail.com>
2024-12-28 19:02:42 +0500

    s6w3 (feat): ignore lint in migrations

--- src/migrations/1735384814289-init-migration.ts
+++ src/migrations/1735384814289-init-migration.ts
@@ -2,7 +2,6 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class InitMigration1735384814289 implements MigrationInterface {
     name = 'InitMigration1735384814289'
-
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "login" character varying(10) NOT NULL, "email" character varying NOT NULL, "password_hash" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_2d443082eccd5198f95f2a36e2c" UNIQUE ("login"), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "public"."likes_status_enum" AS ENUM('Like', 'None', 'Dislike')`);
@@ -15,7 +14,6 @@ export class InitMigration1735384814289 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "posts" ADD CONSTRAINT "FK_7689491fe4377a8090576a799a0" FOREIGN KEY ("blog_id") REFERENCES "blogs"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "users_confirmation_info" ADD CONSTRAINT "FK_5a3f4854acc5803fa69447df8d2" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
-
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "users_confirmation_info" DROP CONSTRAINT "FK_5a3f4854acc5803fa69447df8d2"`);
         await queryRunner.query(`ALTER TABLE "posts" DROP CONSTRAINT "FK_7689491fe4377a8090576a799a0"`);
@@ -28,5 +26,4 @@ export class InitMigration1735384814289 implements MigrationInterface {
         await queryRunner.query(`DROP TYPE "public"."likes_status_enum"`);
         await queryRunner.query(`DROP TABLE "users"`);
     }
-
 }
